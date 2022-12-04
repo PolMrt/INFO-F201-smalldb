1150a7125c31071501b51f24deaafb44e71b6118
RomMarr <romain.markowitch@gmail.com>
2022-12-04 01:02:06 +0100

    [+] fcts bash (attente d'infos pour finir)

--- src/smalldbctl.sh
+++ src/smalldbctl.sh
@@ -1,13 +1,32 @@
 
 
-list(){
+main_process(){
+difpid=`pgrep smalldb`  # process's pid(s)
+declare -a array_main_process  #array with all the main process
+for pid in $difpid
+do
+ret=`pgrep -P $pid | wc -l`            # ret is number of child processes using a wc
+if [ "$ret" -ne 0 ];  #if children !=0
+    then  
+    array_main_process+=("$pid")
+fi
+done
+echo ${array_main_process[@]}
+}
 
+
+list(){
+declare -a list_main_process="$(main_process)"
+for pid in $list_main_process
+do
+kill -SIGUSR2
+done
 }
 
 sync(){
-
+kill -SIGUSR1 ???? pid ?????
 }
 
 stop(){
-    
+kill -SIGINT ???? pid ?????
 }

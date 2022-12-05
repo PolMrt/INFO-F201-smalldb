b90c9561262d2e9c7734a296e515b9231cae134a
Pol Marnette <pol@hey.com>
2022-12-05 11:59:16 +0100

    [~] Start handling singnals

--- src/smalldb.cpp
+++ src/smalldb.cpp
@@ -3,12 +3,14 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <pthread.h>
+#include <signal.h>
 #include <vector>
 #include <iostream>
 #include <string>
 #include <fstream>
 #include <cstdio>
 
+#include "sig.hpp"
 #include "db.hpp"
 #include "queries.hpp"
 #include "socket.hpp"
@@ -16,6 +18,26 @@
 using namespace std;
 
 database_t *share_db;
+vector<int> connections_sockets;
+bool server_stopping = false;
+
+void signal_handler(int signal) {
+  cout << "in sig handler" << endl;
+  if (signal == SIGINT) {
+    server_stopping = true;
+    cout << "smalldb: stopping the server..." << endl;
+
+    // for (int socket_id: connections_sockets) {
+    //   cout << "smalldb: Closing connection " + std::to_string(socket_id) << endl;
+    //   close(socket_id);
+    // }
+
+    // db_save(share_db);
+    exit(EXIT_SUCCESS);
+  } else {
+    return;
+  }
+}
 
 void *thread_fct(void *ptr) {
   int *socket = (int*)ptr;
@@ -34,9 +56,9 @@ void *thread_fct(void *ptr) {
     memset(buffer, 0, 1024);
   }
 
-  if (read_response == 0) {
+  if (read_response == 0 && !server_stopping) {
     cout << "smalldb: Client " + to_string(*socket) + " disconnected (normal). Closing connection and thread" << endl;
-  } else {
+  } else if (!server_stopping) {
     cout << "smalldb: Lost connection to client " + to_string(*socket) << endl;
     cout << "smalldb: Closing connection " + to_string(*socket) << endl;
     cout << "smalldb: Closing thread for connection " + to_string(*socket) << endl;
@@ -64,8 +86,10 @@ int main(int argc, char const *argv[]) {
 
   cout << "smalldb: DB loaded (" + db_path_str + "): " + to_string(share_db->data.size()) + " students in database" << endl;
 
+  // Register signal handler
+  signal(SIGINT, &signal_handler);
 
-  vector<int*> connections_sockets;
+  // Create the server
   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
 
   int opt = 1;
@@ -77,18 +101,23 @@ int main(int argc, char const *argv[]) {
   address.sin_port = htons(28772);
 
   bind(server_fd, (struct sockaddr *)&address, sizeof(address));
-  listen(server_fd, 3); // Max 3 connection waiting
-
-  size_t addrlen = sizeof(address);
+  listen(server_fd, 3);
   
+  size_t addrlen = sizeof(address);
+  sigset_t mask;
+
   while (1) {
-    int *new_socket = new int(accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen));
+    int new_socket = int(accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen));
+
+    block_sig(&mask, SIGINT);
 
     connections_sockets.push_back(new_socket);
     pthread_t tid;
-    pthread_create(&tid, NULL, thread_fct, new_socket);
+    pthread_create(&tid, NULL, thread_fct, &new_socket);
 
-    cout << "smalldb: Accepted connection (" + to_string(*new_socket) + ")" << endl;
+    cout << "smalldb: Accepted connection (" + to_string(new_socket) + ")" << endl;
+
+    unblock_sig(&mask);
   }
   
   close(server_fd);

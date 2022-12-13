a43e6b816cb4e736a126cdebda4a1d8f62fc9c56
Pol Marnette <pol@hey.com>
2022-12-13 15:05:09 +0100

    [~] Fix error when sending nothing to the server

--- src/sdbsh.cpp
+++ src/sdbsh.cpp
@@ -10,14 +10,6 @@
 
 using namespace std;
 
-void check_disconnected_rw(size_t value, int sock) {
-  if (value <= 0) {
-    cout << "sdbsh: lost connection with the server" << endl;
-    close(sock);
-    exit(EXIT_FAILURE);
-  }
-}
-
 int main(int argc, char const *argv[]) {
   if (argc < 2) {
     cout << "sdbsh: error: please provide the ip of the server";
@@ -39,15 +31,28 @@ int main(int argc, char const *argv[]) {
     query_result_t query_result;
 
     int bytes_write = write(sock, request.c_str(), request.length());
-    check_disconnected_rw(bytes_write, sock);
 
+    if (bytes_write < 0) {
+      perror("sdbsh: lost connection with the server\n");
+      close(sock);
+      exit(EXIT_FAILURE);
 
-    char response_buffer[1024];
-    ssize_t bytes_read;
-    while ((bytes_read = read(sock, response_buffer, 1024)) > 0 && strcmp(response_buffer, RESULT_END_MARKER.c_str()) != 0) {
-      std::cout << response_buffer << std::endl;
+      // If bytes write == 0, it means the user tried to send nothing
+    } else if (bytes_write > 0) {
+      char response_buffer[1024];
+      ssize_t bytes_read;
+      
+      while ((bytes_read = read(sock, response_buffer, 1024)) > 0 && strcmp(response_buffer, RESULT_END_MARKER.c_str()) != 0) {
+        std::cout << response_buffer << std::endl;
+      }
+      
+      if (bytes_read <= 0) {
+        perror("sdbsh: lost connection with the server\n");
+        close(sock);
+        exit(EXIT_FAILURE);
+      }
     }
-    check_disconnected_rw(bytes_read, sock);
+
   }
 
   return 0;
